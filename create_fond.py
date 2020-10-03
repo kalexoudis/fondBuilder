2a75debc8f9505330014f9ffa8ac82beeaa6cdf1
FQu99 <61802565+FQu99@users.noreply.github.com>
2020-10-03 15:45:44 +0200

    es läuft jetzt wirklich

--- create_fond.py
+++ create_fond.py
@@ -5,27 +5,23 @@ import pprint
 companies = company_dict.return_company_dictionary()
 
 data = [
-    # ('SAP', 15),
-    ('DAI', 35)
+    ('SAP', 15),
+    ('IBM', 20),
 ]
 
 
-#def test(data):
- #   company = data[0][0]
-  #  print(company)
-   # response_json = time_series_request_adjusted(company)
-    #pp = pprint.PrettyPrinter(indent=4)
-    #pp.pprint(response_json)
-
-
 def build_fond(data):
-    companies = data[0][0]
+    companies = []
+    i = 0
+    for x in data:
+        companies.append(data[i][0])
+        i += 1
     data_points = {}
     for company in companies:
         response = time_series_request_adjusted(company)
         data_points.update({company: []})
         for key in response['Time Series (Daily)']:
-            close = response['Time Series (Daily'][key]['4. close']
+            close = response['Time Series (Daily)'][key]['4. close']
             # for understanding: key equals a date here; create a tupel of a date and a price
             data_point = (key, close)
             data_points[company].append(data_point)
